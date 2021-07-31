67a90cc79c4a73c04e9009bcaa527ca290eea1dd
ephraim_omenai <eomenaiofficial@gmail.com>
2021-07-31 22:04:00 +0100

    added a thankyou page

---
+++ mail.php
@@ -0,0 +1,19 @@
+<?php
+//get data from form  
+
+$name = $_POST['name'];
+$email= $_POST['emailaddress'];
+$message= $_POST['message'];
+
+$to = "eomenaiofficial@gmail.com";
+
+$subject = "Mail From eomenai's website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@eomenai.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
